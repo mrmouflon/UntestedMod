c217d50211bb5d2990d8c2cb336f820ec17929d1
mrmouflon <63329714+mrmouflon@users.noreply.github.com>
2020-04-08 04:38:50 +0100

    Add files via upload

---
+++ Source/MySubModule.cs
@@ -0,0 +1,18 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Library;
+
+namespace UntestedMod
+{
+    public class MySubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarter)
+        {
+            if (game.GameType is Campaign && gameStarter is CampaignGameStarter campaignGameStarter)
+            {
+                campaignGameStarter.LoadGameTexts(BasePath.Name + "Modules/UntestedMod/ModuleData/untested_strings.xml");
+            }
+        }
+    }
+}
